7b16ca22b21fcdf52014f0703fd60781692c72e3
Soumava Seal <sealsoumava@gmail.com>
2025-12-27 22:36:36 +0530

    Development shell added for flutter in nixos

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -4,9 +4,11 @@
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
         zen-browser.url = "github:0xc000022070/zen-browser-flake";
+        flake-utils.url = "github:numtide/flake-utils";
     };
 
-    outputs = inputs@{ self, nixpkgs, ... }: {
+    outputs = inputs@{ self, nixpkgs, flake-utils, ... }: {
+        # NixOS system configuration
         nixosConfigurations = {
             nixos = nixpkgs.lib.nixosSystem {
                 system = "x86_64-linux";
@@ -16,5 +18,27 @@
                 specialArgs = { inherit inputs;};
             };
         };
+
+        # Development shell
+        devShell = flake-utils.lib.eachDefaultSystem (system:
+            let
+                pkgs = import nixpkgs { 
+                    inherit system;
+                    config = {
+                        android_sdk.accept_license = true;
+                        allowUnfree = true;
+                    };
+                };
+                androidSdk = pkgs.androidenv.androidPkgs.androidSdk;
+            in
+            pkgs.mkShell rec {
+                ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
+                buildInputs = with pkgs; [
+                    flutter
+                    androidSdk
+                    javaPackages.compiler.openjdk17
+                ];
+            }
+        );
     }; 
 }
